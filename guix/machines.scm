e63ce5e314c9ed3c4678ec04dc955aa8ab7b67ba
Mathieu OTHACEHE <m.othacehe@gmail.com>
2017-01-16 18:52:22 +0100

    guix: add machines
    Signed-off-by: Mathieu OTHACEHE <m.othacehe@gmail.com>

---
+++ guix/machines.scm
@@ -0,0 +1,7 @@
+(list (build-machine
+        (name "127.0.0.1")
+        (system "x86_64-linux")
+        (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPxTVhCJSxs1Lz3PFWn8elWHrxNH4hgqPdj8Lgh2s6lm")
+        (port 5555)
+        (user "guix-offloader")
+        (speed 2.)))
