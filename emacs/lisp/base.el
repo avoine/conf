b71d769a167ac701762267f053b1a53b8ddb68fa
Mathieu OTHACEHE <mathieu.othacehe@parrot.com>
2017-01-18 11:50:16 +0100

    emacs: guix tramp

--- emacs/lisp/base.el
+++ emacs/lisp/base.el
@@ -21,4 +21,7 @@
 
 (setq tramp-ssh-controlmaster-options
             "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
+(with-eval-after-load 'tramp-sh
+  (push "/run/current-system/profile/bin" tramp-remote-path))
+
 (require 'tramp)
