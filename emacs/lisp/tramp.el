ee6e28894dbe405749e0fb0f22146594c1014aa0
Mathieu Othacehe <m.othacehe@gmail.com>
2017-05-04 22:24:17 +0200

    tramp: add user binaries to path.

--- emacs/lisp/tramp.el
+++ emacs/lisp/tramp.el
@@ -2,7 +2,8 @@
       "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no"
       tramp-hisfile-override "/dev/null")
 (with-eval-after-load 'tramp-sh
-  (push "/run/current-system/profile/bin" tramp-remote-path))
+  (push "/run/current-system/profile/bin" tramp-remote-path)
+  (push "/var/guix/profiles/per-user/mathieu/guix-profile/bin" tramp-remote-path))
 
 (setq tramp-histfile-override "/dev/null")
 
