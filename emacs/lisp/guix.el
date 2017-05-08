c211c3206b7d5f3dda1a284ede85a898a12afb79
Mathieu Othacehe <m.othacehe@gmail.com>
2017-05-08 10:25:49 +0200

    guix.el: remove guix-devel-mode.

--- emacs/lisp/guix.el
+++ emacs/lisp/guix.el
@@ -4,8 +4,6 @@
 (with-eval-after-load 'geiser-guile
   (add-to-list 'geiser-guile-load-path "~/guix"))
 
-(add-hook 'scheme-mode-hook 'guix-devel-mode)
-
 (defun my-run-geiser ()
   (interactive)
   (let ((buf (current-buffer)))
